751e7cc69f63e83046350db4727205bd7829d4c5
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-04-25 03:50:32 -0700

    code started

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
