aff9a257ec17e3f396154311b3e17ed564039743
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-05-04 01:32:29 -0700

    fixed mongodb url to connect

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/socialmedia', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia', {
+  //'mongodb://localhost:27017/socialmedia', {
+    
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
