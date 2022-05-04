cb3940c60342e2c8b959e6d46765a0f93bd738e6
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-05-04 01:36:33 -0700

    fixed date format

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,7 +1,7 @@
 const addDateSuffix = date => {
   let dateStr = date.toString();
 
-  // get last char of date string
+  // Get last char of date string
   const lastChar = dateStr.charAt(dateStr.length - 1);
 
   if (lastChar === '1' && dateStr !== '11') {
@@ -17,7 +17,7 @@ const addDateSuffix = date => {
   return dateStr;
 };
 
-// function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
+// Function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
 module.exports = (
   timestamp,
   { monthLength = 'short', dateSuffix = true } = {}
@@ -70,20 +70,20 @@ module.exports = (
   const year = dateObj.getFullYear();
 
   let hour;
-  // check for 24-hr time
+  // Check for 24-hr time
   if (dateObj.getHours > 12) {
     hour = Math.floor(dateObj.getHours() / 2);
   } else {
     hour = dateObj.getHours();
   }
-  // if hour is 0 (12:00am), change it to 12
+  // If hour is 0 (12:00am), change it to 12
   if (hour === 0) {
     hour = 12;
   }
 
   const minutes = dateObj.getMinutes();
 
-  // set `am` or `pm`
+  // Set `am` or `pm`
   let periodOfDay;
 
   if (dateObj.getHours() >= 12) {
