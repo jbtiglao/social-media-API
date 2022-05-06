628dfb0f1a8783b05b17d820172e1c9fb8c72dcf
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-05-06 03:56:37 -0700

    updated file

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia', {
-  //'mongodb://localhost:27017/socialmedia', {
-    
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialmedia",
+  {
+    //'mongodb://localhost:27017/socialmedia', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
