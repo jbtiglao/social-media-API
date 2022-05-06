0e3e396293a80efabd9ec65a07c670c7843fb268
Jane Tiglao <jane.b.tiglao@gmail.com>
2022-05-06 03:58:08 -0700

    updated files

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,14 +1,14 @@
-const addDateSuffix = date => {
+const addDateSuffix = (date) => {
   let dateStr = date.toString();
 
   // Get last char of date string
   const lastChar = dateStr.charAt(dateStr.length - 1);
 
-  if (lastChar === '1' && dateStr !== '11') {
+  if (lastChar === "1" && dateStr !== "11") {
     dateStr = `${dateStr}st`;
-  } else if (lastChar === '2' && dateStr !== '12') {
+  } else if (lastChar === "2" && dateStr !== "12") {
     dateStr = `${dateStr}nd`;
-  } else if (lastChar === '3' && dateStr !== '13') {
+  } else if (lastChar === "3" && dateStr !== "13") {
     dateStr = `${dateStr}rd`;
   } else {
     dateStr = `${dateStr}th`;
@@ -20,39 +20,39 @@ const addDateSuffix = date => {
 // Function to format a timestamp, accepts the timestamp and an `options` object as optional parameters
 module.exports = (
   timestamp,
-  { monthLength = 'short', dateSuffix = true } = {}
+  { monthLength = "short", dateSuffix = true } = {}
 ) => {
   let months;
 
-  if (monthLength === 'short') {
+  if (monthLength === "short") {
     months = {
-      0: 'Jan',
-      1: 'Feb',
-      2: 'Mar',
-      3: 'Apr',
-      4: 'May',
-      5: 'Jun',
-      6: 'Jul',
-      7: 'Aug',
-      8: 'Sep',
-      9: 'Oct',
-      10: 'Nov',
-      11: 'Dec'
+      0: "Jan",
+      1: "Feb",
+      2: "Mar",
+      3: "Apr",
+      4: "May",
+      5: "Jun",
+      6: "Jul",
+      7: "Aug",
+      8: "Sep",
+      9: "Oct",
+      10: "Nov",
+      11: "Dec",
     };
   } else {
     months = {
-      0: 'January',
-      1: 'February',
-      2: 'March',
-      3: 'April',
-      4: 'May',
-      5: 'June',
-      6: 'July',
-      7: 'August',
-      8: 'September',
-      9: 'October',
-      10: 'November',
-      11: 'December'
+      0: "January",
+      1: "February",
+      2: "March",
+      3: "April",
+      4: "May",
+      5: "June",
+      6: "July",
+      7: "August",
+      8: "September",
+      9: "October",
+      10: "November",
+      11: "December",
     };
   }
 
@@ -87,9 +87,9 @@ module.exports = (
   let periodOfDay;
 
   if (dateObj.getHours() >= 12) {
-    periodOfDay = 'pm';
+    periodOfDay = "pm";
   } else {
-    periodOfDay = 'am';
+    periodOfDay = "am";
   }
 
   const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
